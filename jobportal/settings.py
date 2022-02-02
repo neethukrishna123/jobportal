fcdd53fc6bcde65a7ec0aee2ce2be4109494b89a
Your Name <you@example.com>
2022-02-01 22:24:08 -0800

    version1

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k611^x#o@ef8x6m=1m)r89z5zz19k=@yk0)qhvzn3@==fth4&w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

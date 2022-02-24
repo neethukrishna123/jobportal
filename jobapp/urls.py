be8db40db8d9e2ea1a3782b67d2b3e0bc39d7ee1
Your Name <you@example.com>
2022-02-24 07:20:49 -0800

    version

--- jobapp/urls.py
+++ jobapp/urls.py
@@ -1,6 +1,15 @@
-from. import views
 from django.urls import path
-
+from. import views
 urlpatterns=[
-    path('home',views.home)
+    path('index/',views.fnIndex,name='home'),
+     path('loginpage/',views.fnLoginpage,name='loginpage'),
+     path('registration/',views.fnRegistration,name='registration'),
+     path('about/',views.fnAbout,name='about'),
+     path('contact/',views.fnContact,name='contact'),
+     path('secondregistration/',views.fnSecondregistration,name='secondregistration'),
+     path('joblist/',views.fnJoblist,name='joblist'),
+     path('adminlogin/',views.fnAdminlogin,name='adminlogin'),
+     path('addjob/',views.fnAddjob,name='addjob')
+    
+
 ]
--- jobapp/views.py
+++ jobapp/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request,'one.html')
+
+def fnIndex(request):
+   return render(request,'index.html')
+def fnLoginpage(requ):
+   return render(requ,'loginpage.html')
+def fnRegistration(reque):
+   return render(reque,'Registration.html')
+def fnAbout(reques):
+   return render(reques,'About.html')
+def fnContact(req):
+   return render(req,'Contact.html')
+def fnSecondregistration(req):
+   return render(req,'Secondregistration.html')
+def fnJoblist(req):
+   return render(req,'Joblist.html')
+
+def fnAdminlogin(req):
+   return render(req,'Adminlogin.html')
+def fnAddjob(req):
+   return render(req,'Addjob.html')   
+         
+      
+   

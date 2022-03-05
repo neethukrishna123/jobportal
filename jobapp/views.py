b9a38018f2ee1bdd1a626dc9beb3e476c256b6c7
Your Name <you@example.com>
2022-03-05 00:02:33 -0800

    changed

--- jobapp/views.py
+++ jobapp/views.py
@@ -9,22 +9,22 @@ def fnLoginpage(request1):
 def fnRegistration(request2):
    return render(request2,'Registration.html')
 def fnAbout(request3):
-   return render(request3,'About.html')
+   return render(request3,'about.html')
 def fnContact(request4):
-   return render(request4,'Contact.html')
+   return render(request4,'contact.html')
 def fnSecondregistration(request5):
-   return render(request5,'Secondregistration.html')
+   return render(request5,'secondregistration.html')
 def fnJoblist(request6):
    return render(request6,'joblist.html')
 
 def fnAdminlogin(req):
-   return render(req,'Adminlogin.html')
+   return render(req,'adminlogin.html')
 def fnAddjob(req1):
-   return render(req1,'Addjob.html')  
+   return render(req1,'addjob.html')  
 def fnForgot(req2):
-   return render(req2,'Forgot.html')  
+   return render(req2,'forgot.html')  
 def fnUserpage(req3):
-   return render(req3,'Userpage.html') 
+   return render(req3,'userpage.html') 
          
       
    
